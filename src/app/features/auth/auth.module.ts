9b9ddd930f9ff1f5563e015021da3cdd08ec4fad
MaryemHaytham <maryemhaythamm@gmail.com>
2024-10-27 20:00:46 +0300

    Arch

---
+++ src/app/features/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { OtpComponent } from './components/otp/otp.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    OtpComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
