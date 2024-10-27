7a50cfba16b3b95aa74201886ca037de5ca4b487
MaryemHaytham <maryemhaythamm@gmail.com>
2024-10-27 20:09:13 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { AuthModule } from './features/auth/auth.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { path: '', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { OtpComponent } from './components/otp/otp.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:"login", pathMatch:"full"},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"otp",component:OtpComponent},
+  {path:"forgot-password",component:ForgotPasswordComponent}
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
