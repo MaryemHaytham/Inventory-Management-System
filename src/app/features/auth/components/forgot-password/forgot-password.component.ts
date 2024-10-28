052cf4a03373d204119629b7a46eec3d67a30584
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2024-10-28 23:59:42 +0300

    updating forget component

--- src/app/features/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/components/forgot-password/forgot-password.component.ts
@@ -1,10 +1,32 @@
+
+
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent  {
 
+  ForgetPasswordForm: FormGroup = new FormGroup({
+    email: new FormControl(null, Validators.required),
+   
+  });
+  constructor(private _Router:Router){
+
+  }
+ 
+
+  submitForgetPasswordForm(forgetPasswordFormData: FormGroup) {
+    if (forgetPasswordFormData.valid) {
+      console.log(forgetPasswordFormData.value);
+    }
+  }
+  back(){
+ this._Router.navigateByUrl('login')
+  }
 }
+
