a531ed07b42a50dee28b1b87956e2122cade4bd6
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2024-10-29 20:05:40 +0300

    dashboard

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,12 +18,13 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
   imports: [
     CommonModule,
-    FormsModule, MatFormFieldModule, MatInputModule,ReactiveFormsModule,MatCheckboxModule,MatButtonModule,MatIconModule,
+    RouterModule,
+    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatCheckboxModule, MatButtonModule, MatIconModule,
   ],
-  exports:[
-    FormsModule, MatFormFieldModule, MatInputModule,ReactiveFormsModule,MatCheckboxModule,MatButtonModule,MatIconModule,
+  exports: [
+    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatCheckboxModule, MatButtonModule, MatIconModule,
     NavbarComponent,
     SidebarComponent
   ],
 })
-export class SharedModule {}
+export class SharedModule { }
